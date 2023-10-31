773299a511868556fc0b6c31ff6bb8d4f8f6cce1
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2023-10-30 19:21:52 -0600

    more cleanup

--- src/main/java/IdealizedPageRank.java
+++ src/main/java/IdealizedPageRank.java
@@ -28,107 +28,109 @@ public class IdealizedPageRank {
         SparkConf conf = new SparkConf()
                 .setAppName("IdealizedPageRank");
                 //.setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
 
-        // Load the data
-        JavaRDD<String> lines = sc.textFile(linksFile);
+            // Load the data
+            JavaRDD<String> lines = sc.textFile(linksFile);
 
-        // Parse the data to create the transition matrix
-        JavaPairRDD<Integer, List<Integer>> transitionMatrix = lines.mapToPair(line -> {
-            String[] parts = line.split(":");
-            int from = Integer.parseInt(parts[0].trim());
-            String[] toLinks = parts[1].trim().split(" ");
-            List<Integer> toPages = Arrays.stream(toLinks)
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-            return new Tuple2<>(from, toPages);
-        });
+            // Parse the data to create the transition matrix
+            JavaPairRDD<Integer, List<Integer>> transitionMatrix;
+            transitionMatrix = lines.mapToPair(line -> {
+                String[] parts = line.split(":");
+                int from = Integer.parseInt(parts[0].trim());
+                String[] toLinks = parts[1].trim().split(" ");
+                List<Integer> toPages = Arrays.stream(toLinks)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                return new Tuple2<>(from, toPages);
+            });
 
-        // Define the initial vector
-        int numPages = (int) transitionMatrix.keys().distinct().count();
-        List<Double> initialVector = new ArrayList<>();
-        for (int i = 0; i < numPages; i++) {
-            initialVector.add(1.0 / numPages);
-        }
-
-        // Define the number of iterations
-        int numIterations = 1;
-
-        List<Double> currentVector = initialVector;
-
-        // Perform Idealized PageRank iterations
-        for (int iteration = 0; iteration < numIterations; iteration++) {
-            List<Double> finalCurrentVector = currentVector;
-            JavaPairRDD<Integer, Double> nextVector = transitionMatrix.flatMapToPair(s -> {
-                int from = s._1();
-                List<Integer> toPages = s._2();
-                double rank;
-                if (from >= 1 && from <= finalCurrentVector.size()) {
-                    rank = finalCurrentVector.get(from - 1);
-                } else {
-                    rank = 0.0;
-                }
-                int linkCount = toPages.size();
-                List<Tuple2<Integer, Double>> contribs = new ArrayList<>();
-                for (Integer toPage : toPages) {
-                    contribs.add(new Tuple2<>(toPage, rank / linkCount));
-                }
-                return contribs.iterator();
-            }).reduceByKey((Function2<Double, Double, Double>) Double::sum).mapValues(rank -> 0.15 + 0.85 * rank);
-
-            // Check for convergence
-            boolean hasConverged = true;
+            // Define the initial vector
+            int numPages = (int) transitionMatrix.keys().distinct().count();
+            List<Double> initialVector = new ArrayList<>();
             for (int i = 0; i < numPages; i++) {
-                Map<Integer, Double> nextVectorMap = nextVector.collectAsMap();
-                if (i >= 0 && i < currentVector.size() && nextVectorMap.containsKey(i + 1)) {
-                    double nextValue = nextVectorMap.get(i + 1);
-                    double currentValue = currentVector.get(i);
-                    if (Math.abs(nextValue - currentValue) > 0.0001) {
-                        hasConverged = false;
-                        break;
+                initialVector.add(1.0 / numPages);
+            }
+
+            // Define the number of iterations
+            int numIterations = 1;
+
+            List<Double> currentVector = initialVector;
+
+            // Perform Idealized PageRank iterations
+            for (int iteration = 0; true; iteration++) {
+                List<Double> finalCurrentVector = currentVector;
+                JavaPairRDD<Integer, Double> nextVector = transitionMatrix.flatMapToPair(s -> {
+                    int from = s._1();
+                    List<Integer> toPages = s._2();
+                    double rank;
+                    if (from >= 1 && from <= finalCurrentVector.size()) {
+                        rank = finalCurrentVector.get(from - 1);
+                    } else {
+                        rank = 0.0;
+                    }
+                    int linkCount = toPages.size();
+                    List<Tuple2<Integer, Double>> contribs = new ArrayList<>();
+                    for (Integer toPage : toPages) {
+                        contribs.add(new Tuple2<>(toPage, rank / linkCount));
+                    }
+                    return contribs.iterator();
+                }).reduceByKey((Function2<Double, Double, Double>) Double::sum).mapValues(rank -> 0.15 + 0.85 * rank);
+
+                // Check for convergence
+                boolean hasConverged = true;
+                for (int i = 0; i < numPages; i++) {
+                    Map<Integer, Double> nextVectorMap = nextVector.collectAsMap();
+                    if (i >= 0 && i < currentVector.size() && nextVectorMap.containsKey(i + 1)) {
+                        double nextValue = nextVectorMap.get(i + 1);
+                        double currentValue = currentVector.get(i);
+                        if (Math.abs(nextValue - currentValue) > 0.0001) {
+                            hasConverged = false;
+                            break;
+                        }
                     }
                 }
+
+                if (hasConverged) {
+                    break;
+                }
+
+                currentVector = nextVector.values().collect();
             }
 
-            if (hasConverged) {
-                break;
-            }
+            // Sort the pages by PageRank in descending order
+            JavaPairRDD<Double, Long> sortedPageRanks = sc.parallelize(currentVector)
+                    .zipWithIndex()
+                    .mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()))
+                    .sortByKey(false);
 
-            currentVector = nextVector.values().collect();
+            // Load the page titles
+            JavaRDD<String> titles = sc.textFile(titlesFile);
+
+            // Join the page titles with the final PageRank scores
+            JavaPairRDD<Long, String> pageTitles = titles.zipWithIndex().mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
+
+            // Collect page titles into a map
+            Map<Long, String> pageTitleMap = pageTitles.collectAsMap();
+
+            // Create a broadcast variable for the page titles map
+            Broadcast<Map<Long, String>> pageTitleMapBroadcast = sc.broadcast(pageTitleMap);
+
+            // Use the broadcast variable to retrieve page titles
+            JavaPairRDD<String, Double> joinedData = sortedPageRanks.mapToPair(new PairFunction<Tuple2<Double, Long>, String, Double>() {
+                @Override
+                public Tuple2<String, Double> call(Tuple2<Double, Long> tuple) {
+                    Map<Long, String> titlesMap = pageTitleMapBroadcast.getValue();
+                    String pageTitle = titlesMap.get(tuple._2());
+                    return new Tuple2<>(pageTitle, tuple._1());
+                }
+            });
+
+            // Sorted list of Wikipedia pages based on their PageRank value to a text file
+            joinedData.saveAsTextFile(outputDir);
+
+            // Stop Spark
+            sc.stop();
         }
-
-        // Sort the pages by PageRank in descending order
-        JavaPairRDD<Double, Long> sortedPageRanks = sc.parallelize(currentVector)
-                .zipWithIndex()
-                .mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()))
-                .sortByKey(false);
-
-        // Load the page titles
-        JavaRDD<String> titles = sc.textFile(titlesFile);
-
-        // Join the page titles with the final PageRank scores
-        JavaPairRDD<Long, String> pageTitles = titles.zipWithIndex().mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
-
-        // Collect page titles into a map
-        Map<Long, String> pageTitleMap = pageTitles.collectAsMap();
-
-        // Create a broadcast variable for the page titles map
-        Broadcast<Map<Long, String>> pageTitleMapBroadcast = sc.broadcast(pageTitleMap);
-
-        // Use the broadcast variable to retrieve page titles
-        JavaPairRDD<String, Double> joinedData = sortedPageRanks.mapToPair(new PairFunction<Tuple2<Double, Long>, String, Double>() {
-            @Override
-            public Tuple2<String, Double> call(Tuple2<Double, Long> tuple) {
-                Map<Long, String> titlesMap = pageTitleMapBroadcast.getValue();
-                String pageTitle = titlesMap.get(tuple._2());
-                return new Tuple2<>(pageTitle, tuple._1());
-            }
-        });
-
-        // Sorted list of Wikipedia pages based on their PageRank value to a text file
-        joinedData.saveAsTextFile(outputDir);
-
-        // Stop Spark
-        sc.stop();
     }
 }
--- src/main/java/WikiBomb.java
+++ src/main/java/WikiBomb.java
@@ -27,121 +27,128 @@ public class WikiBomb {
         // Initialize Spark
         SparkConf conf = new SparkConf()
                 .setAppName("WikiBomb");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
 
-        // Load the data
-        JavaRDD<String> lines = sc.textFile(linksFile);
+            // Load the data
+            JavaRDD<String> lines = sc.textFile(linksFile);
 
-        // Modify the link data to create a Wiki Bomb
-        JavaRDD<String> modifiedLines = lines.map(line -> {
-            if (line.contains("surfing")) {
-                return "Rocky Mountain National Park";
-            } else {
-                return line;
-            }
-        });
-
-        // Parse the modified data to create the transition matrix
-        JavaPairRDD<Integer, List<Integer>> transitionMatrix = modifiedLines.mapToPair(line -> {
-            String[] parts = line.split(":");
-            int from = Integer.parseInt(parts[0].trim());
-            String[] toLinks = parts[1].trim().split(" ");
-            List<Integer> toPages = Arrays.stream(toLinks)
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-            return new Tuple2<>(from, toPages);
-        });
-
-        // Define the initial vector
-        int numPages = (int) transitionMatrix.keys().distinct().count();
-        List<Double> initialVector = new ArrayList<>();
-        for (int i = 0; i < numPages; i++) {
-            initialVector.add(1.0 / numPages);
-        }
-
-        // Define the teleportation probability
-        double beta = 0.85;
-
-        // Define the number of iterations
-        int numIterations = 1;
-
-        List<Double> currentVector = initialVector;
-
-        // Perform Taxation PageRank iterations
-        for (int iteration = 0; iteration < numIterations; iteration++) {
-            List<Double> finalCurrentVector = currentVector;
-            JavaPairRDD<Integer, Double> nextVector = transitionMatrix.flatMapToPair(s -> {
-                int from = s._1();
-                List<Integer> toPages = s._2();
-                double rank;
-                if (from >= 1 && from <= finalCurrentVector.size()) {
-                    rank = finalCurrentVector.get(from - 1);
+            // Modify the link data to create a Wiki Bomb
+            JavaRDD<String> modifiedLines = lines.map(line -> {
+                if (line.contains("surfing")) {
+                    return "Rocky Mountain National Park";
                 } else {
-                    rank = 0.0;
+                    return line;
                 }
-                int linkCount = toPages.size();
-                List<Tuple2<Integer, Double>> contribs = new ArrayList<>();
-                for (Integer toPage : toPages) {
-                    contribs.add(new Tuple2<>(toPage, rank / linkCount));
-                }
-                // Add teleportation probability
-                contribs.add(new Tuple2<>(from, (1.0 - beta) / numPages));
-                return contribs.iterator();
-            }).reduceByKey((Function2<Double, Double, Double>) Double::sum).mapValues(rank -> 0.15 + beta * rank);
+            });
 
-            // Check for convergence
-            boolean hasConverged = true;
+            // Parse the modified data to create the transition matrix
+            JavaPairRDD<Integer, List<Integer>> transitionMatrix;
+            transitionMatrix = modifiedLines.mapToPair(line -> {
+                String[] parts = line.split(":");
+                int from = Integer.parseInt(parts[0].trim());
+                String[] toLinks = parts[1].trim().split(" ");
+                List<Integer> toPages = Arrays.stream(toLinks)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                return new Tuple2<>(from, toPages);
+            });
+
+            // Define the initial vector
+            int numPages;
+            numPages = (int) transitionMatrix.keys().distinct().count();
+            List<Double> initialVector;
+            initialVector = new ArrayList<>();
             for (int i = 0; i < numPages; i++) {
-                Map<Integer, Double> nextVectorMap = nextVector.collectAsMap();
-                if (i >= 0 && i < currentVector.size() && nextVectorMap.containsKey(i + 1)) {
-                    double nextValue = nextVectorMap.get(i + 1);
-                    double currentValue = currentVector.get(i);
-                    if (Math.abs(nextValue - currentValue) > 0.0001) {
-                        hasConverged = false;
-                        break;
+                initialVector.add(1.0 / numPages);
+            }
+
+            // Define the teleportation probability
+            double beta = 0.85;
+
+            // Define the number of iterations
+            int numIterations = 1;
+
+            List<Double> currentVector = initialVector;
+
+            // Perform Taxation PageRank iterations
+            for (int iteration = 0; true; iteration++) {
+                List<Double> finalCurrentVector;
+                finalCurrentVector = currentVector;
+                JavaPairRDD<Integer, Double> nextVector;
+                nextVector = transitionMatrix.flatMapToPair(s -> {
+                    int from = s._1();
+                    List<Integer> toPages = s._2();
+                    double rank;
+                    if (from >= 1 && from <= finalCurrentVector.size()) {
+                        rank = finalCurrentVector.get(from - 1);
+                    } else {
+                        rank = 0.0;
+                    }
+                    int linkCount = toPages.size();
+                    List<Tuple2<Integer, Double>> contribs = new ArrayList<>();
+                    for (Integer toPage : toPages) {
+                        contribs.add(new Tuple2<>(toPage, rank / linkCount));
+                    }
+                    // Add teleportation probability
+                    contribs.add(new Tuple2<>(from, (1.0 - beta) / numPages));
+                    return contribs.iterator();
+                }).reduceByKey((Function2<Double, Double, Double>) Double::sum).mapValues(rank -> 0.15 + beta * rank);
+
+                // Check for convergence
+                boolean hasConverged = true;
+                for (int i = 0; i < numPages; i++) {
+                    Map<Integer, Double> nextVectorMap = nextVector.collectAsMap();
+                    if (i >= 0 && i < currentVector.size() && nextVectorMap.containsKey(i + 1)) {
+                        double nextValue = nextVectorMap.get(i + 1);
+                        double currentValue = currentVector.get(i);
+                        if (Math.abs(nextValue - currentValue) > 0.0001) {
+                            hasConverged = false;
+                            break;
+                        }
                     }
                 }
+
+                if (hasConverged) {
+                    break;
+                }
+
+                currentVector = nextVector.values().collect();
             }
 
-            if (hasConverged) {
-                break;
-            }
+            // Sort the pages by PageRank in descending order
+            JavaPairRDD<Double, Long> sortedPageRanks;
+            sortedPageRanks = sc.parallelize(currentVector)
+                    .zipWithIndex()
+                    .mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()))
+                    .sortByKey(false);
 
-            currentVector = nextVector.values().collect();
+            // Load the page titles
+            JavaRDD<String> titles = sc.textFile(titlesFile);
+
+            // Join the page titles with the final PageRank scores
+            JavaPairRDD<Long, String> pageTitles = titles.zipWithIndex().mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
+
+            // Collect page titles into a map
+            Map<Long, String> pageTitleMap = pageTitles.collectAsMap();
+
+            // Create a broadcast variable for the page titles map
+            Broadcast<Map<Long, String>> pageTitleMapBroadcast = sc.broadcast(pageTitleMap);
+
+            // Use the broadcast variable to retrieve page titles
+            JavaPairRDD<String, Double> joinedData = sortedPageRanks.mapToPair(new PairFunction<Tuple2<Double, Long>, String, Double>() {
+                @Override
+                public Tuple2<String, Double> call(Tuple2<Double, Long> tuple) {
+                    Map<Long, String> titlesMap = pageTitleMapBroadcast.getValue();
+                    String pageTitle = titlesMap.get(tuple._2());
+                    return new Tuple2<>(pageTitle, tuple._1());
+                }
+            });
+
+            // Sorted list of Wikipedia pages based on their PageRank value to a text file
+            joinedData.saveAsTextFile(outputDir);
+
+            // Stop Spark
+            sc.stop();
         }
-
-        // Sort the pages by PageRank in descending order
-        JavaPairRDD<Double, Long> sortedPageRanks = sc.parallelize(currentVector)
-                .zipWithIndex()
-                .mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()))
-                .sortByKey(false);
-
-        // Load the page titles
-        JavaRDD<String> titles = sc.textFile(titlesFile);
-
-        // Join the page titles with the final PageRank scores
-        JavaPairRDD<Long, String> pageTitles = titles.zipWithIndex().mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
-
-        // Collect page titles into a map
-        Map<Long, String> pageTitleMap = pageTitles.collectAsMap();
-
-        // Create a broadcast variable for the page titles map
-        Broadcast<Map<Long, String>> pageTitleMapBroadcast = sc.broadcast(pageTitleMap);
-
-        // Use the broadcast variable to retrieve page titles
-        JavaPairRDD<String, Double> joinedData = sortedPageRanks.mapToPair(new PairFunction<Tuple2<Double, Long>, String, Double>() {
-            @Override
-            public Tuple2<String, Double> call(Tuple2<Double, Long> tuple) {
-                Map<Long, String> titlesMap = pageTitleMapBroadcast.getValue();
-                String pageTitle = titlesMap.get(tuple._2());
-                return new Tuple2<>(pageTitle, tuple._1());
-            }
-        });
-
-        // Sorted list of Wikipedia pages based on their PageRank value to a text file
-        joinedData.saveAsTextFile(outputDir);
-
-        // Stop Spark
-        sc.stop();
     }
 }
